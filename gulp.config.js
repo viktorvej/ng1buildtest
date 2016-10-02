fd83638641f6cef05ff3527fc0dad11f31aa8434
Viktor Johansson <viktorvej@gmail.com>
2016-10-02 12:42:39 +0200

    added html script and css injection with gulp

--- gulp.config.js
+++ gulp.config.js
@@ -1,12 +1,48 @@
 module.exports = function() {
     var assets = './src/assets/';
+    var app = './src/app/'
+    var cssDir = assets + 'css/';
 
     var config = {
+        /**
+         * File paths
+         */
+
         //the folder to put the compiled css
-        css: assets + 'css/',
+        cssDir: cssDir,
+
+        //the css file
+        css: cssDir + 'styles.css',
 
         //the less file to compile
-        less: assets + 'styles/styles.less'
+        less: assets + 'styles/styles.less',
+
+        //the dev index html-file
+        index: 'index.html',
+        
+        //all the app js-files
+        js: [
+            app + '**/*.module.js',
+            app + '**/*.js',
+            '!' + app + '**/*.spec.js'
+        ],
+
+        /**
+         * Bower and NPM locations
+         */
+        bower: {
+            json: require('./bower.json'),
+            directory: './bower_components/'
+        }
+    };
+
+    config.getWiredepDefaultOptions = function() {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+        return options;
     };
 
     return config;
--- gulpfile.js
+++ gulpfile.js
@@ -11,16 +11,37 @@ gulp.task('styles', ['clean-styles'], function() {
         .pipe($.plumber())
         .pipe($.less())
         .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
-        .pipe(gulp.dest(config.css));
+        .pipe(gulp.dest(config.cssDir));
 });
 
 gulp.task('clean-styles', function() {
-    var files = config.css + '**/*.css';
+    var files = config.cssDir + '**/*.css';
     clean(files);
 });
 
 gulp.task('less-watcher', function() {
     gulp.watch([config.less], ['styles']);
+});
+
+gulp.task('wiredep', function() {
+    log('Wire up the app css into the html, and call wiredep');
+    var options = config.getWiredepDefaultOptions();
+    var wiredep = require('wiredep').stream;
+
+    return gulp
+        .src(config.index)
+        .pipe(wiredep(options))
+        .pipe($.inject(gulp.src(config.js)))
+        .pipe(gulp.dest(''));
+});
+
+gulp.task('inject', ['wiredep', 'styles'], function() {
+    log('');
+
+    return gulp
+        .src(config.index)
+        .pipe($.inject(gulp.src(config.css)))
+        .pipe(gulp.dest(''));
 });
 
 //////////
